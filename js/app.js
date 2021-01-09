1cd128926ed46fa5ea3ca5b70bd6e828cbe64582
amirahad <arahad4783@gmail.com>
2021-01-10 04:15:07 +0600

    contact page added

--- js/app.js
+++ js/app.js
@@ -22,3 +22,19 @@ $(window).load(function () {
 	afterLoad: function() { }
 	});
 });
+
+
+
+
+function send(e){
+	e.preventDefault();
+	Email.send({
+	SecureToken: "93959c04-81c2-49a5-a3b0-1e0211f569a0",
+    To : 'amirahad420@gmail.com',
+    From : "arahad4783@gmail.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+	}).then(
+		message => alert(message)
+	);
+}
